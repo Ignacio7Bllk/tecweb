fa2678676b417ac456166d0e47c42ffa26f452f6
Ignacio Flores <ignaciofp007@gmail.com>
2024-09-20 14:33:03 -0600

    Correccion ejercicio 1

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -6,7 +6,8 @@
     <title>Práctica 6</title>
 </head>
 <body>
-    <h1>Ejercicio 1</h1>
+
+<h1>Ejercicio 1</h1>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
 
     <form method="GET">
@@ -14,18 +15,11 @@
         <input type="submit" value="Enviar">
     </form>
 <?php
-    // Incluir el archivo con la función
-    include 'src/funciones.php';
-
+    
     if (isset($_GET['numero'])) {
         $num = $_GET['numero'];
-
-    if (Multiplo($num)) {
-        echo '<h3>R= El número ' . $num . ' SÍ es múltiplo de 5 y 7.</h3>';
-    } else {
-        echo '<h3>R= El número ' . $num . ' NO es múltiplo de 5 y 7.</h3>';
+    Multiplo($num);
     }
-}
 ?>
 
 </body>
--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -1,9 +1,15 @@
 <?php
 function Multiplo($num) {
+
+if(is_numeric($num)){
+
+
     if ($num % 5 == 0 && $num % 7 == 0) {
-        return true; 
+        echo '<h3>R= El número ' . $num . ' SÍ es múltiplo de 5 y 7.</h3>';
     } else {
-        return false; 
+        echo '<h3>R= El número ' . $num . ' NO es múltiplo de 5 y 7.</h3>';
     }
+
+}
 }
 ?>

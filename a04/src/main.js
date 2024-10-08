e91b065954edf40ac3cce39748301879ee73cf28
Ignacio Flores <ignaciofp007@gmail.com>
2024-10-08 15:54:29 -0600

    Creacion de carpeta a04

---
+++ a04/src/main.js
@@ -0,0 +1,10 @@
+/*function getDatos(){
+    var nombre = prompt("Ingrese su nombre");
+    var edad = prompt("Ingrese su edad");
+
+    var div1 = document.getElementById("nombre");
+    div1.innerHTML = '<h3>Nombre: ' + nombre + '</h3>';
+
+    var div2 = document.getElementById("edad");
+    div2.innerHTML = '<h3>Edad: ' + edad + '</h3>';
+}*/

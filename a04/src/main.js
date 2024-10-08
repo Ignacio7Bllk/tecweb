7a770026d84f4f64cff97f8abee678a27ea7a4f8
Ignacio Flores <ignaciofp007@gmail.com>
2024-10-08 17:05:36 -0600

    Termine ejercicio 1

--- a04/src/main.js
+++ a04/src/main.js
@@ -8,3 +8,10 @@
     var div2 = document.getElementById("edad");
     div2.innerHTML = '<h3>Edad: ' + edad + '</h3>';
 }*/
+
+
+function ejercicio1(){
+    var ejercicio1 = document.getElementById("ejercicio1");
+    ejercicio1.innerHTML = '<h3>Hola mundo</h3>';
+
+}
